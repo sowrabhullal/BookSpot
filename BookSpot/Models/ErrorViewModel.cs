5b1bd72c5aa1378fc24ae99e204f3a46ad4d20d8
sullal <sowrabh.ullal@tektronix.com>
2024-09-28 18:32:55 +0530

    mvc project issues fixed

---
+++ BookSpot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookSpot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
